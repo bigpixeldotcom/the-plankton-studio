2ce04744e33a693d5e49353b7451c0e62c1c5eae
adrian-beston <adrian@big-pixel.com>
2025-12-14 10:04:05 +0000

    Add FontAwesome + kit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,15 +1,14 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.SANITY_PROJECT_ID
+const dataset = process.env.SANITY_DATASET
+
 export default defineCliConfig({
   api: {
-    projectId: 'vvhbjths',
-    dataset: 'production'
+    projectId,
+    dataset
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
     autoUpdates: true,
   }
 })
